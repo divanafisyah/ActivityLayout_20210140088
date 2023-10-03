ad260eaf271af048d8e2476e63213cad82a4d279
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2023-10-03 20:13:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/project290923/framelayout.kt
@@ -0,0 +1,11 @@
+package com.example.project290923
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class framelayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_framelayout)
+    }
+}
